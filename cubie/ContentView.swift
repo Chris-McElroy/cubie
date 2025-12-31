f24a0f820a12406ceabc12f14a4f4e16c76a3967
ChrisMcElroy <csmcelroy@me.com>
2025-12-31 11:28:07 -0800

    got demo zoom in effect working, deciding against it

--- cubie/ContentView.swift
+++ cubie/ContentView.swift
@@ -8,75 +8,92 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let layer: Int
+    let colorA: Color
+    let colorB: Color
+    
+    
     @State var sizeA: CGFloat = 100
     @State var sizeB: CGFloat = 100
     
     
-    @State var zIndexA: CGFloat = 2
-    @State var zIndexB: CGFloat = 1
+    @State var zIndexA: CGFloat = 3
+    @State var zIndexB: CGFloat = 3
     
     var body: some View {
         ZStack() {
-            Rectangle()
-                .foregroundStyle(Color.green)
-                .frame(width: sizeA, height: sizeA)
-                .padding(.trailing, 500-sizeA/2)
-                .padding(.all, 100-sizeA/2)
-                .zIndex(zIndexA)
-            Rectangle()
-                .foregroundStyle(Color.blue)
-                .frame(width: sizeB, height: sizeB)
-                .padding(.leading, 500-sizeB/2)
-                .padding(.all, 100-sizeB/2)
-                .zIndex(zIndexB)
-            Button(action: changeSizes) {
-                Color.red
-                    .overlay {
-                        Text("press me")
+            Color.black.zIndex(-10)
+            if layer == 1 {
+                ContentView(layer: 2, colorA: .pink, colorB: .black)
+                    .zIndex(zIndexA+CGFloat(layer)*10)
+                    .mask {
+                        Rectangle()
+                            .foregroundStyle(Color.blue)
+                            .frame(width: sizeA, height: sizeA)
+                            .padding(.trailing, 500-sizeA/2)
+                            .padding(.all, 100-sizeA/2)
+                            .zIndex(zIndexA+5+CGFloat(layer)*10)
                     }
             }
+            Rectangle()
+                .stroke(colorA, lineWidth: 50)
+                .frame(width: sizeA-50, height: sizeA-50)
+                .padding(.trailing, 500-sizeA/2)
+                .padding(.all, 100-sizeA/2)
+                .zIndex(zIndexA+1+CGFloat(layer)*10)
+                .onTapGesture {
+                    zIndexA = 6
+                    zIndexB = 3
+                    withAnimation(.easeIn(duration: 0.3)) {
+                        sizeA = 1000
+                    }
+                }
+            if layer == 1 {
+                ContentView(layer: 2, colorA: .purple, colorB: .purple)
+                    .zIndex(zIndexB+CGFloat(layer)*10)
+                    .mask {
+                        Rectangle()
+                            .foregroundStyle(Color.blue)
+                            .frame(width: sizeB, height: sizeB)
+                            .padding(.leading, 500-sizeB/2)
+                            .padding(.all, 100-sizeB/2)
+                            .zIndex(zIndexB+CGFloat(layer)*10)
+                    }
+            }
+            Rectangle()
+                .stroke(colorB, lineWidth: 50)
+                .frame(width: sizeB-50, height: sizeB-50)
+                .padding(.leading, 500-sizeB/2)
+                .padding(.all, 100-sizeB/2)
+                .zIndex(zIndexB+1+CGFloat(layer)*10)
+                .onTapGesture {
+                    zIndexB = 6
+                    zIndexA = 3
+                    withAnimation(.easeIn(duration: 0.3)) {
+                        sizeB = 1000
+                    }
+                }
+            Button(action: restoreSizes) {
+                Color.red
+                    .overlay {
+                        Text("back")
+                    }
+            }
+            .zIndex(100)
             .buttonStyle(PlainButtonStyle())
             .focusEffectDisabled()
             .frame(width: 100, height: 40)
             .padding(.top, 400)
         }
-        .padding()
-        .onAppear {
-        }
     }
     
-    func changeSizes() {
-        withAnimation(.easeIn(duration: 1.0)) {
-            sizeA = 1000
-        }
-        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: false) { _ in
-            withAnimation(.easeIn(duration: 1.0)) {
-                sizeA = 100
-            }
-        }
-        Timer.scheduledTimer(withTimeInterval: 2.0, repeats: false) { _ in
-            zIndexA = 1.0
-            zIndexB = 2.0
-            withAnimation(.easeIn(duration: 1.0)) {
-                sizeB = 1000
-            }
-        }
-        Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false) { _ in
-            withAnimation(.easeIn(duration: 1.0)) {
-                sizeB = 100
-            }
-        }
-        Timer.scheduledTimer(withTimeInterval: 4.0, repeats: false) { _ in
-            zIndexB = 1.0
-            zIndexA = 2.0
-            withAnimation(.easeIn(duration: 1.0)) {
-                sizeA = 1000
-            }
-        }
-        Timer.scheduledTimer(withTimeInterval: 5.0, repeats: false) { _ in
-            withAnimation(.easeIn(duration: 1.0)) {
-                sizeA = 100
-            }
+    func restoreSizes() {
+        zIndexA = 3
+        zIndexB = 3
+        withAnimation(.easeOut(duration: 0.2)) {
+            sizeA = 100
+            sizeB = 100
         }
     }
 }
