c81ded4671141c046113432c5dbe27d12f75c6f4
ChrisMcElroy <csmcelroy@me.com>
2026-01-11 08:12:47 -0500

    testing realitykit

--- cubie/AlgSets/AlgMenuView.swift
+++ cubie/AlgSets/AlgMenuView.swift
@@ -6,6 +6,56 @@
 //
 
 import SwiftUI
+import RealityKit
+
+struct TestView: View {
+    var body: some View {
+        RealityView { content in
+            let cube = MeshResource.generateBox(size: 0.5, cornerRadius: 0.02)
+            let entity = ModelEntity(mesh: cube, materials: [SimpleMaterial(color: .blue, isMetallic: false)])
+            entity.components.set(InputTargetComponent(allowedInputTypes: .all))
+                    
+            
+            entity.transform.rotation = simd_quatf(angle: .pi/4, axis: [0,1,0])
+            entity.transform.rotation *= simd_quatf(angle: .pi/4.5, axis: normalize(SIMD3<Float>(1.0, 0.0, 1.0)))
+//            entity.transform = Transform(pitch: -Float(Angle.degrees(10).radians))
+            let plane1 = ModelEntity(mesh: .generatePlane(width: 0.4, height: 0.4), materials: [SimpleMaterial(color: .red, isMetallic: false)])
+            plane1.transform.translation = SIMD3<Float>(x: 0, y: 0, z: 0.26)
+            plane1.generateCollisionShapes(recursive: false)
+            plane1.name = "red"
+            entity.addChild(plane1)
+            
+            
+            let plane2 = ModelEntity(mesh: .generatePlane(width: 0.4, height: 0.4), materials: [SimpleMaterial(color: .green, isMetallic: false)])
+            plane2.transform.translation = SIMD3<Float>(x: 0, y: 0.26, z: 0)
+            plane2.transform.rotation = simd_quatf(angle: -.pi/2, axis: [1,0,0])
+//            let component = GestureComponent(DragGesture())
+            plane2.generateCollisionShapes(recursive: false)
+            plane2.name = "green"
+//            component.canDrag = true
+//            plane2.components.set(component)
+            entity.addChild(plane2)
+            
+            let plane3 = ModelEntity(mesh: .generatePlane(width: 0.4, height: 0.4), materials: [SimpleMaterial(color: .yellow, isMetallic: false)])
+            plane3.transform.translation = SIMD3<Float>(x: -0.26, y: 0, z: 0)
+            plane3.transform.rotation = simd_quatf(angle: -.pi/2, axis: [0,1,0])
+            plane3.generateCollisionShapes(recursive: false)
+            plane3.name = "yellow"
+            entity.addChild(plane3)
+            
+            content.add(entity)
+        }
+        .gesture(DragGesture().targetedToAnyEntity().onChanged { path in
+//            print(path.entity.name)
+            print(path.entity(at: path.location, in: .local)?.name ?? "none")
+        })
+        .gesture(TapGesture().targetedToAnyEntity().onEnded { path in
+//            print(path.entity.name)
+            print(path.entity.name)
+            
+        })
+    }
+}
 
 struct AlgMenuView: View {
     @State var header: String = Storage.main.currentAlgSet?.header ?? ""
@@ -99,9 +149,9 @@ struct AlgMenuView: View {
                 .border(.tertiary, width: 3)
             if popupType == .addNewMenu {
                 EditMenuOption(menuOption: options[selectedItem ?? 0] as? SubMenuOptionView ?? SubMenuOptionView(title: "", header: "", subOptions: []))
-                    .onDisappear {
-                        options[selectedItem] = ??
-                    }
+//                    .onDisappear {
+//                        options[selectedItem] = ??
+//                    }
             } else if popupType == .copyExistingMenu {
                 
             } else if popupType == .addNewAlg {
--- cubie/cubieApp.swift
+++ cubie/cubieApp.swift
@@ -6,14 +6,21 @@
 //
 
 import SwiftUI
+import RealityKit
 
 @main
 struct cubieApp: App {
-    var body: some Scene {
+    @State var angle: CGFloat = 45.0
+    
+    var body: some SwiftUI.Scene {
         WindowGroup {
             ZStack {
                 Color.black
-                AlgMenuView()
+                TestView()
+                VStack {
+                    Text("testing")
+                    Spacer()
+                }
             }
         }
     }
